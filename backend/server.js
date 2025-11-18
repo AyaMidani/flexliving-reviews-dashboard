3ef863759a54ec82ad5dc88d384f5ac92ce91aa5
Aya Mydani <ayamidani98@gmail.com>
2025-11-19 02:01:45 +0300

    Port deploy problem fix

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
